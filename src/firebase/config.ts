91753f69bd119e7930aa8c8beb9e0e925eb4dc0e
Huxleigh Services <service@huxleigh.com>
2026-03-06 03:02:07 +0000

    4. If using Firebase Studio / environment variables Make sure your .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:627500824058:web:5cca81d73b6f6176b6a043",
   "apiKey": "AIzaSyBTsSZ4cH7B4Wp5-Rv1XTYbzn0yqwxHg3E",
   "authDomain": "studio-9029052952-9df3f.firebaseapp.com",
-  "storageBucket": "feedcyba",
+  "storageBucket": "studio-9029052952-9df3f.appspot.com",
   "measurementId": "",
   "messagingSenderId": "627500824058"
 };
